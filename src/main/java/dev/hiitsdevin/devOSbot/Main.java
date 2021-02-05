e8d9c18d4c2cb1cc747d67f42a77616c07eb399d
Devin <devin@hiitsdevin.dev>
2021-02-05 11:24:37 -0800

    Trying to get a listener working

--- src/main/java/dev/hiitsdevin/devOSbot/Main.java
+++ src/main/java/dev/hiitsdevin/devOSbot/Main.java
@@ -3,6 +3,12 @@ package dev.hiitsdevin.devOSbot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.security.auth.login.LoginException;
@@ -11,13 +17,13 @@ public class Main {
     private static final Yaml yaml = new Yaml();
     public static JDA jdaInstance;
     
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) throws Exception {
         Runtime r = Runtime.getRuntime();
 
         r.addShutdownHook(new Thread() {
             public void run() {
                 jdaInstance.shutdown();
-                System.out.println("haha nuka cola'd- oh fuCK");
+                System.out.println("Bot shutting down...");
             }
         });
 
@@ -25,7 +31,30 @@ public class Main {
 
         JDABuilder builder = JDABuilder.createDefault(config.token);
         builder.setActivity(Activity.watching("devOS live... barely."));
+        builder.addEventListeners(new MyListener());
 
         jdaInstance = builder.build();
+
+        jdaInstance.awaitReady();
+    }
+
+    public static class MyListener extends ListenerAdapter {
+        public void onMessageRecieved(MessageReceivedEvent event) {
+            if (event.getAuthor().isBot()) return;
+
+            Message message = event.getMessage();
+            String content = message.getContentDisplay();
+
+            if (content.equals("d!ping")) {
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("Ponged! I am in devOS!").queue();
+            }
+        }
+    }
+
+    public void onEvent(GenericEvent event) {
+        if (event instanceof ReadyEvent) {
+            System.out.println("API is up and ready!");
+        }
     }
 }
